0780ac16a19d0aa3176c61a882835943cf1619e0
Monil Gandhi <monil.gandhi@volansys.com>
2023-01-03 15:42:36 +0530

    Add list builder

--- expanse_planner/lib/main.dart
+++ expanse_planner/lib/main.dart
@@ -32,20 +32,22 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Flutter App'),
       ),
-      body: Column(
-        // mainAxisAlignment: MainAxisAlignment.spaceAround,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          const SizedBox(
-            width: double.infinity,
-            child: Card(
-              color: Colors.blue,
-              elevation: 5,
-              child: Text('CHART!'),
+      body: SingleChildScrollView(
+        child: Column(
+          // mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: const [
+            SizedBox(
+              width: double.infinity,
+              child: Card(
+                color: Colors.blue,
+                elevation: 5,
+                child: Text('CHART!'),
+              ),
             ),
-          ),
-          UserTransactions()
-        ],
+            UserTransactions()
+          ],
+        ),
       ),
     );
   }
--- expanse_planner/lib/widgets/transaction_list.dart
+++ expanse_planner/lib/widgets/transaction_list.dart
@@ -7,37 +7,41 @@ class TransactionList extends StatelessWidget {
   const TransactionList(this.transactions, {super.key});
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: transactions.map((tx) {
-        return Card(
-          child: Row(
-            children: [
-              Container(
-                margin:
-                    const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                decoration: BoxDecoration(
-                    border: Border.all(color: Colors.purple, width: 2)),
-                padding: const EdgeInsets.all(10),
-                child: Text('\$${tx.amount}',
-                    style: const TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                        color: Colors.purple)),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(tx.title,
+    return SizedBox(
+      height: 500,
+      child: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return Card(
+            child: Row(
+              children: [
+                Container(
+                  margin:
+                      const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.purple, width: 2)),
+                  padding: const EdgeInsets.all(10),
+                  child: Text('\$${transactions[index].amount}',
                       style: const TextStyle(
-                          fontWeight: FontWeight.bold, fontSize: 16)),
-                  Text(DateFormat.yMMMd().format(tx.date),
-                      style: const TextStyle(color: Colors.grey))
-                ],
-              )
-            ],
-          ),
-        );
-      }).toList(),
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.purple)),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(transactions[index].title,
+                        style: const TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 16)),
+                    Text(DateFormat.yMMMd().format(transactions[index].date),
+                        style: const TextStyle(color: Colors.grey))
+                  ],
+                )
+              ],
+            ),
+          );
+        },
+        itemCount: transactions.length,
+      ),
     );
   }
 }
