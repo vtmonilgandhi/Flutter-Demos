99deb6d0092e262d772040e78b26658fae00ad0c
Monil Gandhi <monil.gandhi@volansys.com>
2023-01-03 16:55:38 +0530

    minor changes

--- expanse_planner/lib/main.dart
+++ expanse_planner/lib/main.dart
@@ -14,11 +14,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Personal Expenses',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Personal Expenses'),
     );
   }
 }
